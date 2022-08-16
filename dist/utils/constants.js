15e50bcf106cf029cd020edec6387948d1b4029c
Jennie-geo <isintumejenny@gmail.com>
2022-08-16 17:42:28 +0100

    remove dist folder

--- dist/utils/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SQL_ERRORS = void 0;
-var SQL_ERRORS;
-(function (SQL_ERRORS) {
-    SQL_ERRORS[SQL_ERRORS["ER_DUP_ENTRY"] = 1062] = "ER_DUP_ENTRY";
-})(SQL_ERRORS = exports.SQL_ERRORS || (exports.SQL_ERRORS = {}));
-//# sourceMappingURL=constants.js.map
